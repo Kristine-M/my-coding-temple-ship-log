7cb49ccee90b337d935c76ad61a0d6978d59e696
Kristine-M <kma28@uic.edu>
2024-02-04 16:55:23 -0600

    added updated python file

--- new_adv_log.py
+++ new_adv_log.py
@@ -1,7 +1,6 @@
 def greet_coding_temple():
-    print("hi traveler")
+    print("hi traveler, from mondstadt")
     
 if __name__ == "__main__":
     greet_coding_temple()
     
-    print("testing git")
